e43c93f67ac2350883b2d0d5296f7e1d0869f34b
ehsanmolaei991 <ehsanmolaei991@yahoo.com>
2020-07-24 22:15:24 +0430

    add some filed  & schema

--- extensions/users-permissions/config/schema.graphql.js
+++ extensions/users-permissions/config/schema.graphql.js
@@ -3,11 +3,13 @@ module.exports = {
     extend type UsersPermissionsMe {
       name: String
       family: String
+      phone: String
       balance: Int
+      address: [ComponentAddressItem]
       advertises(sort: String, limit: Int, start: Int, where: JSON): [Advertise]
       orders(sort: String, limit: Int, start: Int, where: JSON): [Order]
       boosts(sort: String, limit: Int, start: Int, where: JSON): [Boost]
       favoriteDishes(sort: String, limit: Int, start: Int, where: JSON): [Dish]
       payments(sort: String, limit: Int, start: Int, where: JSON): [Payment]
-    }`
-}
+    }`,
+};
