be30de58589c0685cd4567df650093383922b96b
force <force1267@gmail.com>
2020-09-17 10:02:33 +0430

    added nodemailer

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+    email: {
+        provider: 'nodemailer',
+        providerOptions: {
+            host: env('SMTP_HOST', 'localhost'),
+            port: env('SMTP_PORT', 25),
+            auth: {
+                user: env('SMTP_USERNAME'),
+                pass: env('SMTP_PASSWORD'),
+            }
+            // ... any custom nodemailer options
+        },
+        settings: {
+            defaultFrom: 'support@hayatemoon.com',
+            defaultReplyTo: 'support@hayatemoon.com',
+        }
+    }
+});
