212284930ad1f255eaaff5660c743dd29601881b
force <force1267@gmail.com>
2020-09-17 13:16:56 +0430

    unsecre nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -2,12 +2,14 @@ module.exports = ({ env }) => ({
     email: {
         provider: 'nodemailer',
         providerOptions: {
-            host: env('SMTP_HOST', 'localhost'),
+            host: env('SMTP_HOST', '192.168.150.10'),
             port: env('SMTP_PORT', 25),
             auth: {
                 user: env('SMTP_USERNAME'),
                 pass: env('SMTP_PASSWORD'),
             },
+            secure: false,
+            ignoreTLS: true,
             tls: {
                 rejectUnauthorized: false
             }
